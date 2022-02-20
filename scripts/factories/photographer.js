d4660bc974b15fcae8aee64195176c82576a8e1c
Mxtgy <maxime.tanguy22@gmail.com>
2022-02-20 22:30:51 +0900

    Mise en place
    Installation du projet + mise en place de ESLint + Arborescence + Page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const {
+    name, portrait, id, city, country, tagline, price,
+  } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement('article');
+    const link = document.createElement('a');
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
+    const h2 = document.createElement('h2');
+    const location = document.createElement('span');
+    const tag = document.createElement('span');
+    const priceDay = document.createElement('span');
+    link.setAttribute('href', `photographer.html?${id}`);
+    img.setAttribute('src', picture);
+    location.setAttribute('class', 'location');
+    tag.setAttribute('class', 'tagline');
+    priceDay.setAttribute('class', 'price');
+    h2.textContent = name;
+    location.textContent = `${city}, ${country}`;
+    tag.textContent = tagline;
+    priceDay.textContent = `${price}€/jour`;
+    article.appendChild(link);
+    link.appendChild(figure);
+    figure.appendChild(img);
+    link.appendChild(h2);
+    article.append(location, tag, priceDay);
+    return (article);
+  }
+  return { name, picture, getUserCardDOM };
 }
+
+export default photographerFactory;
